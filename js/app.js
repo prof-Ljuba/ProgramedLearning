3e967591b8fa63b8a912ab42884f8fa7a1ca6b08
Ljubisa Cvetic <cveticljubisa@yahoo.com>
2026-01-16 00:58:24 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,19 @@
 let currentLanguage = "sr";  //sr ili en za sada
-let currentLessonFile = "osnove_racunara.json"; //osnove_racunara.json ili computer_basics.json za sada
+let currentLessonFile = "racunarstvo/introductionComputerScience.json"; //osnove_racunara.json ili computer_basics.json za sada
 let lessonData = null;
 
 // Učitaj lekciju iz JSON fajla
 async function loadLesson(language, lessonFile) {
-  const path = `lessons/${language}/${lessonFile}`;
+  const path = `lessons/${lessonFile}`;
 
-  try {
+ try {
     const response = await fetch(path);
-    if (!response.ok) {
-      throw new Error("Lesson not found");
-    }
+    if (!response.ok) throw new Error("Lesson not found");
 
     lessonData = await response.json();
     showLessonInfo();
   } catch (error) {
-    console.error("Greška pri učitavanju lekcije:", error);
+    console.error(error);
     alert("Lekcija ne može da se učita: " + path);
   }
 }
