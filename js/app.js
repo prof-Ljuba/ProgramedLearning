fa243a95be49237f52cc3584f3353b0b680dfc52
Ljubisa Cvetic <cveticljubisa@yahoo.com>
2026-01-17 21:03:57 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -25,10 +25,10 @@ async function loadLesson(language, lessonFile) {
 function showLessonInfo() {
   document.body.innerHTML = `
     <h1><strong>Naslov lekcije: </strong> ${lessonData.sr.naslov}</h1>
-    <p><strong>Naslov prozora: </strong> ${lessonData.it.naslovProzora}</p>
-    <p><strong>naziv Dela Lekcije: </strong> ${lessonData.sr.deloviLekcije.nazivDelaLekcije}</p>
-    <hr>
-    <p><strong>Teorija / 1.deo :</strong> ${lessonData.sr.deloviLekcije.teorija[0].tekst}</p>
+    //<p><strong>Naslov prozora: </strong> ${lessonData.it.naslovProzora}</p>
+    //<p><strong>naziv Dela Lekcije: </strong> ${lessonData.sr.deloviLekcije.nazivDelaLekcije}</p>
+    //<hr>
+    //<p><strong>Teorija / 1.deo :</strong> ${lessonData.sr.deloviLekcije.teorija[0].tekst}</p>
   `;
 }
 
