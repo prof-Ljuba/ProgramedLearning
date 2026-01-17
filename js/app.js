cc4b9c3a2a8776b6e30fa0486a166b3bf0b90247
Ljubisa Cvetic <cveticljubisa@yahoo.com>
2026-01-17 22:37:57 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ function showLessonInfo() {
     <p><strong>id:</strong> ${lessonData.id}</p>
     <p><strong>sr.naslov:</strong> ${lessonData.sr.naslov}</p>
     <hr>
-    <p><strong>Comment:</strong> ${lessonData.sr.deloviLekcije.nazivDelaLekcije}</p>
+    <p><strong>sr.deloviLekcije.nazivDelaLekcije:</strong> ${lessonData.sr.deloviLekcije.nazivDelaLekcije}</p>
   `;
 }
 
